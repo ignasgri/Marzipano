dbf34c1fbee7f9464215cac2993fd6750bfe801b
Ignas Grigaravicius <kickeris@gmail.com>
2017-09-09 08:16:47 +0100

    Test of marzipano

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img2",
+      "name": "img2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 0.12247797248228665,
+        "pitch": 0.1851398985540964,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.157360776884099,
+          "pitch": 0.2656636656618545,
+          "title": "<span style=\"color: rgb(34, 34, 34); font-family: arial, sans-serif; font-size: 13px; background-color: rgb(255, 255, 255);\">Lough Tay</span>",
+          "text": "<span style=\"color: rgb(34, 34, 34); font-family: arial, sans-serif; background-color: rgb(255, 255, 255);\">Lough Tay, commonly called The Guinness Lake, is a small but scenic lake set on private property, in the Wicklow Mountains in County Wicklow, Ireland</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
